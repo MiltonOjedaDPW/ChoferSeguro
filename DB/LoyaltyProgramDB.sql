7a7ff44f65d519c202644ee1aa59e4b385dee0d9
Milton Ojeda <Milton.Ojeda@dpworld.com.do>
2021-05-04 14:27:15 -0400

    commit 3

---
+++ DB/LoyaltyProgramDB.sql
@@ -0,0 +1,8 @@
+select * from vEmpleados where LOCATION_CODE = 'People Department'
+
+
+
+SELECT * FROM vEmpleados WHERE JOB_NAME LIKE '%MANAGER%'
+
+
+SELECT * FROM vEmpleados WHERE JOB_NAME LIKE '%DIREC%'
